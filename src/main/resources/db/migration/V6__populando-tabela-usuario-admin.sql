382ca6da5e9acc50dc38c89b69657413ed6f3a5d
Weslley Rocha <weslley.nx@gmail.com>
2026-03-07 13:23:09 -0300

    fix: updated senha.

--- src/main/resources/db/migration/V6__populando-tabela-usuario-admin.sql
+++ src/main/resources/db/migration/V6__populando-tabela-usuario-admin.sql
@@ -2,10 +2,10 @@
 INSERT INTO usuarios (login, senha, ativo)
 VALUES
 ('admin', '$2a$10$M5gWFbUsk4q68VoeASwlt.q155mI.LiI6eTwEGXCmKDXs4uTC9G8y', 1),
-('Wes_Rocha', '$2a$10$7snb3JkD4SVrpjjUAxqbueh7PunpWWFQrW1qOnGOD2dUA8oiWSi3a', 1),
-('Weslley_Silva', '$2a$10$IvIanpuog/WEw.WFODMFAOOA//bpLihO/zIko/FLwMX2aLIo9KGAm', 0),
-('Guilherme_Motta', '$2a$10$wgIcmI935yWzlxlIJ7WumOG6Fnm2LDzdg4Ze0SyJ5gvCwIYjVq4Wa', 0),
-('Marcos_Vitor', '$2a$10$aJc00i0Ec9kWdV2toSSlGuMq.e9pnftBL.xRdkWBq4kEE8mJMwZh6', 0),
-('Wesley_Silva', '$2a$10$pAk1VGHmMxVvTO9yk.I9m.Uk/tbLbUqe5.ElDD1OIRx5UySin4FrO', 0),
-('Sandra Freitas', '$2a$10$WaGuSrcaULDSLhnC0ExmtO48asuAdl2fqE2ClUp3Tqd4R1JAj.D06', 1),
-('Esther Freitas','$2a$10$v2GpKn0nnQBJaHUodbm5j.x8PvucAgeMmKJvEp6TsTSD6xfq1aA6a', 1);
+('Wes_Rocha@gmail.com', '$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 1),
+('Weslley_Silva@gmail.com', '$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 0),
+('Guilherme_Motta@gmail.com', '$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 0),
+('Marcos_Vitor@gmail.com', '$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 0),
+('Wesley_Silva@gmail.com', '$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 0),
+('Sandra_Freitas@gmail.com', '$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 1),
+('Esther_Freitas@gmail.com','$2a$10$fv8Peu96VZ66YUD0yFsYYOtwXzFfgJn7UaoiE6Ro2RbxFNOb89fSm', 1);
