55dd2ef97c7a32c4c66d2375aebb7b769a65f908
Weslley Rocha <weslley.nx@gmail.com>
2026-03-14 15:53:11 -0300

    Add Exeption

---
+++ src/main/java/br/com/senai/s042/autoescolas042/Domain/Instrucao/InstrutorIndisponivelException.java
@@ -0,0 +1,9 @@
+package br.com.senai.s042.autoescolas042.Domain.Instrucao;
+
+public class InstrutorIndisponivelException extends RuntimeException {
+
+    public InstrutorIndisponivelException(String message) {
+
+        super(message);
+    }
+}
